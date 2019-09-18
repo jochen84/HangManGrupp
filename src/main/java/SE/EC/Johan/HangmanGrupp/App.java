2e760d6ccc28817fc055f1849796e9548a5a5dc1
jochen84 <simman@rocketmail.com>
2019-09-18 16:15:47 +0200

    Tagit bort lite System.exit med mera...

--- src/main/java/SE/EC/Johan/HangmanGrupp/App.java
+++ src/main/java/SE/EC/Johan/HangmanGrupp/App.java
@@ -6,10 +6,10 @@ public class App {
     private static Scanner scanner = new Scanner(System.in);
 
     public static HangMan hangman = new HangMan();
-
+    public static boolean keepAlive;
     public static void main( String[] args ) {
 
-        StartGame.startGame();
+        //StartGame.startGame();
         hangman.newGame();
         playGame();
 
@@ -18,32 +18,39 @@ public class App {
     public static void playGame(){
 
         System.out.println("\nOk, då spelar vi! Det hemliga ordet består av: " + hangman.getWordToFind().length() + " bokstäver, lycka till!");
-        for(char word : hangman.getSecretWordArray()) {
-            System.out.print(word + " ");
-        }
 
-        boolean keepAlive = true;
-        while(keepAlive && hangman.getUsedGuesses() <= hangman.getMaxGuesses()){
-            System.out.println("\nGissa på en boktsav eller hela ordet (" + hangman.getWordToFind().length() + ") bokstäver:");
-
-            String input = scanner.nextLine().toUpperCase();
-            String charInput = input.substring(0,1);
-            
-            if (hangman.getWordToFind().equals(input)) {
-                System.out.println("Rätt gissat, spelet är slut");
-                keepAlive = false;
-            }
-            else{
-                enterLetters(charInput);}
-
-            for(char word : hangman.getSecretWordArray()) {
+        keepAlive = true;
+        while (keepAlive && hangman.getUsedGuesses() <= hangman.getMaxGuesses()) {
+            for (char word : hangman.getSecretWordArray()) {
                 System.out.print(word + " ");
             }
-            if(hangman.isWordCorrect()){
-                System.out.println("\nGrattis, du har hittat rätt ord! Spelet är slut!\n Rätta ordet var: " + hangman.getWordToFind());
+            System.out.println("\nGissa på en boktsav eller hela ordet (" + hangman.getWordToFind().length() + ") bokstäver:             [-Använda bokstäver:" + hangman.getUsedLetters() + "-]");
+
+            String input = scanner.nextLine().toUpperCase();
+            String charInput = input.substring(0, 1);
+
+            if (hangman.getWordToFind().equals(input)) {
+                System.out.println("Rätt gissat! Ordet var: " + hangman.getWordToFind() + "\nSpelet är slut!");
+                keepAlive = false;
+            } else if (input.length() < hangman.getWordToFind().length() && input.length() > 1) {
+                System.out.println("Ordet du gissade är för kort, ordet består av " + hangman.getWordToFind().length() + " bokstäver.");
+            } else if (input.length() > hangman.getWordToFind().length()) {
+                System.out.println("Ordet du gissade är för långt, ordet består av " + hangman.getWordToFind().length() + " bokstäver.");
+            } else if (input.length() == hangman.getWordToFind().length()/* && !hangman.getWordToFind().equals(input)*/){ //Behöver inte && satset? Kommer inte hit ändå??
+                System.out.println("Ordet du gissade är fel, försök igen.\nGissning: " + hangman.getUsedGuesses() + "/" + hangman.getMaxGuesses());
+                int guesses = hangman.getUsedGuesses();
+                hangman.setUsedGuesses(++guesses);
+                if (hangman.getUsedGuesses() > hangman.getMaxGuesses()) {
+                    System.out.println("Du förlorade! Alla gissningar är slut...\nDet rätta ordet var: " + hangman.getWordToFind());
+                    keepAlive = false;
+                }
+            }else{
+                enterLetters(charInput);
+            }
+            if (hangman.isWordCorrect()) {
+                System.out.println("\nGrattis, du har hittat rätt ord! Spelet är slut!\nRätta ordet var: " + hangman.getWordToFind());
                 keepAlive = false;
             }
-            System.out.println("\nAnvända bokstäver: " + hangman.getUsedLetters());
         }
     }
 
@@ -64,7 +71,8 @@ public class App {
                 hangman.setUsedGuesses(++guesses);
                     if(hangman.getUsedGuesses() > hangman.getMaxGuesses()){
                         System.out.println("Du förlorade! Alla gissningar är slut...\nDet rätta ordet var: " + hangman.getWordToFind());
-                        System.exit(0);
+                        keepAlive = false;
+                        // System.exit(0);
                     }
             }
         }else{
